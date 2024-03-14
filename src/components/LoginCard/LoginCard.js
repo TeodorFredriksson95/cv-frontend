ef1ae5014fb0573503e1ffac1c8d9b6efe980bfd
Teodor Fredriksson <teo95@hotmail.se>
2024-03-14 22:59:53 +0100

    auth service connection

--- src/components/LoginCard/LoginCard.js
+++ src/components/LoginCard/LoginCard.js
@@ -1,20 +1,39 @@
 import "./LoginCard.css";
 import React, { useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
+import axios from "axios";
 
 const LoginCard = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    window.google.accounts.id.initialize({
+      client_id: "590785779954-5gusittjkdj2ci5tf5d5ker9nnqimdju.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+    });
+  }, []);
+
+  const handleCredentialResponse = async (response) => {
+    console.log("Encoded JWT ID token from Google: " + response.credential);
+    const authResponse = await axios.post(process.env.REACT_APP_AUTHENTICATION_SERVICE_BASE_URL + "/api/authenticate/google", {
+      token: response.credential,
+    });
+    console.log("jwt token from auth service: ", authResponse.data.token);
+  };
+  const handleSignInClick = () => {
+    if (window.google.accounts.id) {
+      window.google.accounts.id.prompt();
+    }
+  };
   return (
     <div>
       <Navbar />
-
+      <div id="g_id_onload" data-client_id="590785779954-5gusittjkdj2ci5tf5d5ker9nnqimdju.apps.googleusercontent.com" data-prompt_parent_id="g_id_onload"></div>
       <div className="login-card-container">
         <h2 className="section-title-h3 login-card-title">Login</h2>
         <div className="login-img-logo">
           <img src="physicalCVlogo.png" />
         </div>
         <div className="oAuth-providers-container">
-          <div className="oauth-provider-flex-container google-button">
+          <div className="oauth-provider-flex-container google-button" onClick={handleSignInClick}>
             <div className="icon-wrapper">
               <div id="google-icon">
                 <span className="devicon--google"></span>
@@ -40,7 +59,7 @@ const LoginCard = () => {
             <div className="oauth-provider-flex-container linkedin-button">
               <div className="icon-wrapper">
                 <div id="linkedin-icon">
-                  <span class="devicon--linkedin"></span>
+                  <span className="devicon--linkedin"></span>
                 </div>
               </div>
               <div className="oauth-text linkedin">
