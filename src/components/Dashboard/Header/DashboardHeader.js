a5dd49c66edf57e2036356a2898918417ad24df3
Teodor Fredriksson <teo95@hotmail.se>
2024-03-17 15:30:02 +0100

    dashboard wip

--- src/components/Dashboard/Header/DashboardHeader.js
+++ src/components/Dashboard/Header/DashboardHeader.js
@@ -6,7 +6,23 @@ import axios from "axios";
 
 const DashboardHeader = () => {
   const [profileImage, setProfileImage] = useState("");
-  const [apiAccesKey, setApiAccessKey] = useState("");
+  const [apiAccessKey, setApiAccessKey] = useState("");
+  const [isCTCBVisible, setIsCTCBVisible] = useState(false);
+
+  const handleCopyToClipboard = async () => {
+    if (apiAccessKey) {
+      try {
+        await navigator.clipboard.writeText(apiAccessKey);
+        console.log(isCTCBVisible);
+        setIsCTCBVisible(true);
+        setTimeout(() => {
+          setIsCTCBVisible(false);
+        }, "5000");
+      } catch (error) {
+        console.error("Failed to copy API key:", error);
+      }
+    }
+  };
 
   const handleButtonFetchApi = async () => {
     const token = localStorage.getItem("authToken");
@@ -49,14 +65,31 @@ const DashboardHeader = () => {
               <h4 className="section-title-h4">Generate Api Key</h4>
             </div>
             <div className="generate-api-key-bar">
-              <input id="api-key-input" type="text" value={apiAccesKey} readOnly placeholder="[your-personal-api-key]"></input>
-              <div>
-                <span className="gg--clipboard"></span>
+              <div className="input-notification-container">
+                <input id="api-key-input" type="text" value={apiAccessKey} readOnly placeholder="[your-personal-api-key]"></input>
+                {isCTCBVisible && <div id="copied-notification">Copied to clipboard!</div>}
               </div>
+              <span id="copy-to-clipboard" onClick={handleCopyToClipboard} className="gg--clipboard"></span>
             </div>
             <div className="generate-key-button">
               <button onClick={handleButtonFetchApi}>Generate Key</button>
             </div>
+          </div>
+          <div id="personal-api-key-info-container">
+            <p>
+              By generating an API key and including it in your authorization header as a bearer token you will gain instant access to the Unidevweb API. For more information about how to use the API,
+              please visit the{" "}
+              <a className="links" href="/doc">
+                documentaion
+              </a>{" "}
+              page.
+              <span id="separate-dashboard-info">
+                Remember to always store your API key in a safe in order to protect it's integrity. You can read more about best practices regarding storage of sensitive information{" "}
+                <a className="links" href="/linktobestpractices">
+                  here.
+                </a>
+              </span>
+            </p>
           </div>
         </div>
       </div>
